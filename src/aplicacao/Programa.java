b109e930a5f357bd71d309f461b48f9ce6b33028
Thiago Vanzele <thiagovanzele@gmail.com>
2022-11-03 17:52:31 -0300

    Primeira classe: Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,13 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		Posicao p = new Posicao(3, 5);
+		System.out.println(p.toString());
 	}
 
 }
